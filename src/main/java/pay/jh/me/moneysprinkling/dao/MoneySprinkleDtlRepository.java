8a09e836732da514b763b6b5b7dc27a9088884a9
junghoon-sin <junghoon.sin@nts-corp.com>
2020-07-20 20:51:50 +0900

    가져오기 api 개선 > 트랜잭션 및 jpa 처리

--- src/main/java/pay/jh/me/moneysprinkling/dao/MoneySprinkleDtlRepository.java
+++ src/main/java/pay/jh/me/moneysprinkling/dao/MoneySprinkleDtlRepository.java
@@ -4,6 +4,9 @@ package pay.jh.me.moneysprinkling.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pay.jh.me.moneysprinkling.model.MoneySprinkleDtl;
 
+import java.util.Optional;
+
 public interface MoneySprinkleDtlRepository extends JpaRepository<MoneySprinkleDtl, String> {
 
+    MoneySprinkleDtl findTop1ByToken(String token);
 }
--- src/main/java/pay/jh/me/moneysprinkling/service/MoneySprinklingServiceImpl.java
+++ src/main/java/pay/jh/me/moneysprinkling/service/MoneySprinklingServiceImpl.java
@@ -2,6 +2,7 @@ package pay.jh.me.moneysprinkling.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import pay.jh.me.moneysprinkling.dao.MoneySprinkleDtlRepository;
 import pay.jh.me.moneysprinkling.dao.MoneySprinkleRepository;
@@ -9,6 +10,7 @@ import pay.jh.me.moneysprinkling.model.MoneySprinkle;
 import pay.jh.me.moneysprinkling.model.MoneySprinkleDtl;
 import pay.jh.me.moneysprinkling.util.TokenGenerator;
 
+import java.util.Optional;
 import java.util.Random;
 
 import static org.springframework.transaction.annotation.Isolation.SERIALIZABLE;
@@ -45,10 +47,11 @@ public class MoneySprinklingServiceImpl implements MoneySprinklingService {
     }
 
     @Override
-    @Transactional(isolation = SERIALIZABLE)
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
     public double pickup(String token, String userId) {
-        MoneySprinkleDtl moneySprinkleDtl = null; // 테이블 MONEY_SPRINKLE_DTL에서 reviver 가 null 인 데이터 가져오기
-        //moneySprinkleDtlRepository.update(moneySprinkleDtl.getMoneySprinkleDtlSeq(), userId); receiver, receiver at 업데이트
+        MoneySprinkleDtl moneySprinkleDtl = moneySprinkleDtlRepository.findTop1ByToken(token);
+        moneySprinkleDtl.setReceiver(userId);
+        moneySprinkleDtlRepository.save(moneySprinkleDtl);
         return moneySprinkleDtl.getAmount();
     }
 
